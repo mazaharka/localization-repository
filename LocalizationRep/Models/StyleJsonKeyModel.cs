e06d1c15448fd8fb964cc3fd038e1d5aeaf451f0
Захар Клочков <zklochkov@clevertec.ru>
2020-08-25 12:42:26 +0300

    no message

---
+++ LocalizationRep/Models/StyleJsonKeyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LocalizationRep.Models
+{
+    public class StyleJsonKeyModel
+    {
+        public int ID { get; set; }
+        public string StyleName { get; set; }
+        public List<LangKeyModel> LangKeyModels { get; set; }
+
+        public MainTable MainTables { get; set; }
+    }
+}
